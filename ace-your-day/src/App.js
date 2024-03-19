aafd74fa3b2976494d88a858138013a5b7e58cca
Ananya Sista <sistaa@ufl.edu>
2024-03-19 12:44:03 -0400

    starting setting up react router

--- ace-your-day/src/App.js
+++ ace-your-day/src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
+      <Router></Router>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
