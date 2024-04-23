731d6cbd3ef88a12dcceda00e2433751d12ec9cf
Cameron Brown <me@cbrxyz.com>
2024-04-23 14:10:09 -0400

    commit for my sistaa

--- backend/backend/serializers.py
+++ backend/backend/serializers.py
@@ -1,3 +1,4 @@
+from bson import ObjectId
 from rest_framework import serializers
 
 from .models import Event, User
@@ -12,8 +13,15 @@ class UserSerializer(serializers.ModelSerializer):
 class EventSerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
         r = super().to_representation(instance)
-        r["owner"] = UserSerializer(instance.owner).data
+        if isinstance(instance.owner, User):
+            r["owner"] = UserSerializer(instance.owner).data
         return r
+
+    def to_internal_value(self, data):
+        data = data.copy()
+        if isinstance(data["owner"], str):
+            data["owner"] = ObjectId(data["owner"])
+        return super().to_internal_value(data)
 
     class Meta:
         model = Event
